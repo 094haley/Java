2adc579a1f7da576adfd6418996de8e885759580
094haley <094haley@gmail.com>
2022-08-22 19:29:33 +0900

    2022/08/22 수업

--- Backjoon/src/step02/_04.java
+++ Backjoon/src/step02/_04.java
@@ -1,10 +1,36 @@
 package step02;
 
 /*
- * 날짜 : 2022/08/19
+ * 날짜 : 2022/08/22
  * 이름 : 이해빈
- * 내용 : 백준 1단계 1번 문제. Hello World
+ * 내용 : 백준 2단계 4번 문제. 사분면 고르기
  */
-public class _04 {
 
+import java.util.Scanner;
+
+public class _04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a > 0 && b > 0 ) {
+			
+			System.out.println(1);
+			
+		}else if(a < 0 && b > 0 ) {
+			
+			System.out.println(2);
+			
+		}else if(a < 0 && b < 0 ) {
+		
+			System.out.println(3);
+		
+		}else if (a > 0 && b <0 ) {
+		
+			System.out.println(4);
+		}	
+		
+		sc.close();
+	}
 }
--- Backjoon/src/step02/_05.java
+++ Backjoon/src/step02/_05.java
@@ -3,8 +3,17 @@ package step02;
 /*
  * 날짜 : 2022/08/19
  * 이름 : 이해빈
- * 내용 : 백준 1단계 1번 문제. Hello World
+ * 내용 : 백준 2단계 5번 문제. 알람시계
  */
-public class _05 {
 
+import java.util.Scanner;
+
+public class _05 {
+	public static void main(String[] args) {
+	Scanner	sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	
+	
+	}
 }
