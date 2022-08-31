216014021c77ec694250708c87bd33c9cc395f94
094haley <094haley@gmail.com>
2022-08-31 12:20:07 +0900

    2022/08/31 집

--- Backjoon/src/step04/_03.java
+++ Backjoon/src/step04/_03.java
@@ -1,12 +1,14 @@
 package step04;
 
 /*
- * 날짜 : 2022/08/28
+ * 날짜 : 2022/08/31
  * 이름 : 이해빈
  * 내용 : 백준 4단계 3번 문제. 나머지
  */
 
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class _03 {
 	public static void main(String[] args) throws IOException {
@@ -15,22 +17,22 @@ public class _03 {
 		
 		
 		int [] arr = new int [10];
-		int count = 1;
+		int count = 0;
 		
 		for(int i = 0; i < 10; i ++) {
-			
 			int a = Integer.parseInt(br.readLine());
 			arr[i] = a % 42;
 		}
 		
 		for(int i = 0; i < 10; i ++) {
-			
-			for(int j = 0; j < 10; j ++) {
-				
+			int b = 0;
+			for(int j = i+1; j < 10; j ++) {
 				if (arr[i] == arr[j]) {
-					
+					b++;
 				}
-				
+			}
+			if(b == 0) {
+				count++;
 			}
 		}
 		
--- Backjoon/src/step04/_04.java
+++ Backjoon/src/step04/_04.java
@@ -1,11 +1,45 @@
 package step04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
- * 날짜 : 2022/08/28
+ * 날짜 : 2022/08/31
  * 이름 : 이해빈
- * 내용 : 백준 4단계 4번 문제.
+ * 내용 : 백준 4단계 4번 문제. 평균
  */
 
 public class _04 {
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr1 = new int [n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < n; i ++) {
+			int a = Integer.parseInt(st.nextToken());
+			arr1[i] = a;
+		} // 배열 
+		
+		
+		int m = arr1[0];
+		for (int i = 0; i < n ; i ++) {
+			if(m < arr1[i]) {
+				m = arr1[i];
+			}
+		} // 최댓값
+		
+		int [] arr2 = new int [n];
+		for (int i = 0; i < n; i ++) {
+			arr2[i] = arr1[i] / m * 100;
+		} // 고친성적
+		
+		System.out.println();
+		
+	}
 }
