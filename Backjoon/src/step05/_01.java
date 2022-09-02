d95e7c2d68e4e8f05499a74054a64cef65f66bfb
094haley <094haley@gmail.com>
2022-09-02 19:49:43 +0900

    2022/09/02 학원

--- Backjoon/src/step05/_01.java
+++ Backjoon/src/step05/_01.java
@@ -1,5 +1,18 @@
 package step05;
 
+/*
+ * 날짜 : 2022/09/02
+ * 이름 : 이해빈
+ * 내용 : 백준 5단계 1번 문제. 정수 N개의 합
+ */
+
 public class _01 {
 
+	    long sum(int[] a) {
+	        long ans = 0;
+	        for(int i = 0; i < a.length; i ++){
+	            ans += a[i];
+	        }
+	        return ans;
+	    }
 }
